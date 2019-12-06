6ac97f7d216a6db6d927c940bb8da85befd83fc0
Yevhen Konyk <yevhen.konyk@gmail.com>
2019-12-06 20:44:59 +0200

    Added React Conditional Rendering examples

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import Product2 from './Product';
 import Panel from './Panel';
 import ProFileDetails from './Profile';
+import MailBox from './MailBox';
 
 const App = () => (
   //React Fragment - так как для рендеринга нескольких элементов нужен общий родитель.
   <>
+    <MailBox unreadMessages={[1,2,3]}></MailBox>
     <Panel title="Profile Details">
       <ProFileDetails name="Yevhen Konyk" email="yevhen.konyk@gmail.com"/>
     </Panel>
-
+    
     <Product2
       imageURL="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
       // alt="Tacos With Lime"
