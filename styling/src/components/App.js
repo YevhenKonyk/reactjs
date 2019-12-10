a4864a742050e8d057e4ee021c2d7b89c90eca85
Yevhen Konyk <yevhen.konyk@gmail.com>
2019-12-10 16:24:01 +0200

    Added examples with CSS modules

--- styling/src/components/App.js
+++ styling/src/components/App.js
@@ -6,11 +6,13 @@ import Products from '../products.json';
 import ProductList from './ProductList';
 import ButtonWithInlineStyles from './Button';
 import ButtonWithVanilaCSS from './Button/Button';
+import ButtonWithCSSModules from './Button/module/Button';
 
 
 const App = () => (
   //React Fragment - так как для рендеринга нескольких элементов нужен общий родитель.
   <>
+    <ButtonWithCSSModules label='Button With CSS Modules'/>
     <ButtonWithInlineStyles label="Button with inline styles"/>
     <ButtonWithVanilaCSS label="ButtonWithVanilaCSS"/>
     <Panel title="Best Sellers">
